4536416000ab908a8bb8a50bb755fee22d409c58
Harry <official1fade@gmail.com>
2024-09-18 09:04:08 -0400

    responsive mobile menu

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ function copyMenu() {
 
     // copy inside nav to nav
     var mainNav = document.querySelector('.header-nav nav');
+    var navPlace= document.querySelector('.off-canvas nav');
+    navPlace.innerHTML =mainNav.innerHTML;
 
+    //copy .header-top .wrapper to .thetop-nav
+    var topNav = document.querySelector('.header-top .wrapper');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav')
+    topPlace.innerHTML = topNav.innerHTML;
 }
 copyMenu()
+
+//show sub menu on mobile
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e){
+    e.preventDefault();
+    submenu.forEach((item)=> item != this ? item.closest('.has-child'))
+}
